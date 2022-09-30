8b57b5e27b9b9c47335c21c0a77f7810610671f1
Alex13101 <podiyachiy-a@mail.ru>
2022-09-30 19:57:33 +0300

    Задача с числами

---
+++ Задачка.py
@@ -0,0 +1,10 @@
+a=int(input("Введите целое число "))
+b=int(input("Введите целое число "))
+sum=a+b
+raznost=a-b
+proizv=a*b
+print("Сумма чисел=",sum)
+print("Разность чисел=",raznost)
+print("произведение чисел=",proizv)
+
+

4c362b7ac8e4e7d94410aa79c6d9b1e28dbbeddd
Alex13101 <podiyachiy-a@mail.ru>
2022-09-30 21:04:41 +0300

    поместил в папку

---
+++ Занятие 30.09.22/30.09.2022.py
@@ -0,0 +1,3 @@
+my_string="1234"
+my_int=int(my_string)
+print(my_int)
---
+++ Занятие 30.09.22/Задачка.py
@@ -0,0 +1,18 @@
+a=int(input("Введите целое число 1 "))
+b=int(input("Введите целое число 2 "))
+
+sum=a+b
+raznost=a-b
+proizv=a*b
+
+print("Сумма чисел=",sum)
+print("Разность чисел=",raznost)
+print("произведение чисел=",proizv)
+if b!=0:
+    delen=a\b
+    print("Результат деления=",delen)
+else:
+    print("Нельзя делить на 0")
+
+
+

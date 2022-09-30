ef4ce03bc0f2b0f4ec91e4c4f5d06d927ccb1561
Alex13101 <podiyachiy-a@mail.ru>
2022-09-30 20:46:24 +0300

    Задача с числами

---
+++ Задача 3.py
@@ -0,0 +1,13 @@
+a=int(input("Введите целое число 1 "))
+b=int(input("Введите целое число 2 "))
+c=int(input("Введите целое число 3 "))
+if a<=b<=c:
+    print(a,b,c)
+elif b<=c<=a:
+    print(b,c,a)
+elif c<=a<=b:
+    print(c,a,b)
+elif b<=a<=c:
+    print(b,a,c)
+
+

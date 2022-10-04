fccf366d0f95b7e789ddb432eac02d74931d1c2a
Alex13101 <podiyachiy-a@mail.ru>
2022-10-04 20:21:17 +0300

    Списки sqare и четные

--- Занятие 04.10.22/Задача1.py
+++ Занятие 04.10.22/Задача1.py
@@ -1,10 +1,19 @@
-n = int(input())
+"""n = int(input())
 my_list = []
 for i in range(n):
     number = int(input())
     my_list.append(number)
 
 for i in my_list:
-    print(i*i)
-
-
+    print(i*i)"""
+a = [int(input()) for i in range(int(input()))]
+q = []
+even = []
+#a = [int(x) for x in input().split()]
+for i in range(len(a)):
+    t = a[i]
+    q.append(t*t)
+    if t%2 ==0:
+        even.append(t)
+print(q, even, sep = '\n')
+   #print("Четные числа  ",n , "=", i)

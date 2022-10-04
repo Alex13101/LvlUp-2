43121964ec5d71eef1c7c6381461528686191eb5
Alex13101 <podiyachiy-a@mail.ru>
2022-10-04 20:16:13 +0300

    Списки sqare

--- Занятие 04.10.22/Задача1.py
+++ Занятие 04.10.22/Задача1.py
@@ -3,5 +3,8 @@ my_list = []
 for i in range(n):
     number = int(input())
     my_list.append(number)
-    print(number * number)
-#print(i*2 for i in range (my_list)
+
+for i in my_list:
+    print(i*i)
+
+

313cf9e480d6a2f19fa9ea1030c6b029d14c8aae
Alex13101 <podiyachiy-a@mail.ru>
2022-10-04 22:10:16 +0300

    Strings

---
+++ Занятие 04.10.22/строки.py
@@ -0,0 +1,17 @@
+text=input()
+print(text.count(" "))
+x=[]
+n = int(input("Введите кол-во слов  "))
+sum_string=""
+for i in range(n):
+    sum_string += input()
+    sum_string+=" "
+
+
+print(f"{sum_string}.")
+
+
+
+
+
+

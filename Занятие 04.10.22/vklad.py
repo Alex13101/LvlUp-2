5e8c30df33267df985bd0591f1482400b50805ad
Alex13101 <podiyachiy-a@mail.ru>
2022-10-04 21:19:08 +0300

    functuon vkla

---
+++ Занятие 04.10.22/vklad.py
@@ -0,0 +1,12 @@
+def read_input():
+    a = int(input("введите сумму "))
+    b = int(input("Введите срок  "))
+    c=int(input("Введите процент "))
+    return a,b,c
+
+def operations(a,b,c):
+    d=a*c*b
+    print("Ваша сумма через ",b,"лет будет ", d)
+
+a,b,c=read_input()
+operations(a,b,c)

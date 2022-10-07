d73720f2feda4e245e7c76fee99d806ba0f6f9bc
Alex13101 <podiyachiy-a@mail.ru>
2022-10-07 13:04:29 +0300

    Обратный порядок введенных чисел

---
+++ Homework1/Task3.py
@@ -0,0 +1,9 @@
+n=input("Ведите кол-во чисел: ").split()
+my_list=[]
+#for i in range(n):
+    #number=int(input("Вводим числа: "))
+    #my_list.append(number)
+my_list.reverse()
+n.reverse()
+print(n)
+print([my_list])

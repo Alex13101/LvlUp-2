ef8d9ea9c1f640418c3eb7c8ac99414a0b92c670
Alex13101 <podiyachiy-a@mail.ru>
2022-10-10 16:12:40 +0300

    Change first and end in string

--- Занятие 07.10.22/T1.py
+++ Занятие 07.10.22/T1.py
@@ -1,9 +1,13 @@
+
+"""Напишите функцию change(lst), которая
+принимает список и меняет местами его первый и
+последний элемент.В исходном списке минимум 2 элемента."""
+
+
+
 def change (my_list):
     my_list[0], my_list[-1]=my_list[-1], my_list[0]
     print(my_list)
-
-
-    #print(my_list)
 
 n=int(input())
 my_list=[]
@@ -11,7 +15,4 @@ for i in range(n):
     number=int(input())
     my_list.append(number)
 print(my_list)
-#my_list.reverse()
-
-#print(my_list)
 change(my_list)

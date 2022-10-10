37dbfa8012c0d46503be71508205fd220a066460
Alex13101 <podiyachiy-a@mail.ru>
2022-10-10 17:31:30 +0300

    Unic elements

---
+++ Занятие 07.10.22/T3.py
@@ -0,0 +1,36 @@
+"""Дан список, упорядоченный по неубыванию элементов в
+нем. Определите, сколько в нем различных элементов."""
+
+
+
+
+n=input("Ведите кол-во чисел через пробел: ").split()
+lst=[]
+for i in range (len(n)):
+    lst.append(int(n[i]))
+    lst.count(i)
+lst.sort()
+print(lst)
+a=0
+saw=[]
+for i in lst:
+    if i not in saw:
+        a+=1
+        saw.append(i)
+print(a)
+for i in saw:
+    print(f"Элемент {i} встречается {lst.count(i)} раз")
+
+
+
+
+
+
+
+#     print(lst.count(i))
+#     if lst.count(i)==1:
+#         a+=1
+#         print(i,"Встречается один раз")
+#     elif lst.count(i)>1:
+#         print(i,"Встречается много раз")
+# print(a,"уникальных элементов")

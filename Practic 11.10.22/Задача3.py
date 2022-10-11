ceed800f428a54e34ee88a65054e46ee56e11807
Alex13101 <podiyachiy-a@mail.ru>
2022-10-11 21:43:39 +0300

    mean(среднее)

--- Practic 11.10.22/Задача3.py
+++ Practic 11.10.22/Задача3.py
@@ -4,9 +4,9 @@
 обходимо вывести список чисел ниже среднего, равных ему (если такие
 найдутся) и выше среднего. Каждый список должен предваряться соот-
 ветствующим заголовкоm"""
-from utils import read_input
-my_list=read_input()
-print(my_list)
+# from utils import read_input
+# my_list=read_input()
+# print(my_list)
 
 def read_input():
     n = int(input("введите число"))
@@ -15,14 +15,14 @@ def read_input():
       number = int(input())
       my_list.append(number)
     return my_list
+if __name__ == "__main__":
+    my_list = read_input()
+    import statistics
+    mean_number = statistics.mean(my_list)
+    print("Среднее значение", mean_number)
 
-my_list = read_input()
-import statistics
-mean_number = statistics.mean(my_list)
-print("Среднее значение", mean_number)
-
-for i in my_list:
-    if i >= mean_number:
-        print("Выше среднего", i)
-    elif i < mean_number:
-        print("Ниже среднего", i)
+    for i in my_list:
+        if i >= mean_number:
+            print("Выше среднего", i)
+        elif i < mean_number:
+            print("Ниже среднего", i)

a52b257f7fe1df2ae4afefd27fc60c42684f2f2b
Alex13101 <podiyachiy-a@mail.ru>
2022-10-11 21:41:58 +0300

    Tests def

--- Homework1/Task3.py
+++ Homework1/Task3.py
@@ -1,9 +1,12 @@
 n=input("Ведите кол-во чисел: ").split()
-my_list=[]
+my_list=n[::-1]
 #for i in range(n):
     #number=int(input("Вводим числа: "))
     #my_list.append(number)
-my_list.reverse()
+#my_list.reverse()
 n.reverse()
+
+
 print(n)
-print([my_list])
+
+print(my_list)

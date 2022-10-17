c2f796ec17b198259d40bbf4f7d451cd30f2ac81
Alex13101 <podiyachiy-a@mail.ru>
2022-10-17 17:59:10 +0300

    Different Functions

--- utils.py
+++ utils.py
@@ -4,23 +4,51 @@ def read_input():
     for i in range(len(n)):
         lst.append(int(n[i]))
     return lst
-#lst=read_input()
-#print(lst)
+
+
+def read_input2():
+    n = int(input("Ведите кол-во чисел: "))
+    my_list = []
+    for i in range(n):
+        number = int(input("Ведите число: "))
+        my_list.append(number)
+    return my_list
+
+
+
+# lst=read_input()
+# print(lst)
 from tqdm import tqdm
+
+
 def find_dividers(n):
-    dividers=[]
-    for i in tqdm(range(2,n)):
-        if n%i==0:
+    dividers = []
+    for i in tqdm(range(2, n)):
+        if n % i == 0:
             dividers.append(i)
     print(dividers)
 
 
-def resolve_try_type(a,b,c):
-    if a==c==b:
-        return("Треугольник равноcторонний")
-    elif a==b!=c or b==c!=a or b!=a==c or c!=a==b:
+def resolve_try_type(a, b, c):
+    if a == c == b:
+        return ("Треугольник равноcторонний")
+    elif a == b != c or b == c != a or b != a == c or c != a == b:
         return ("Треугольник равнобедренный")
-    elif a!=b!=c!=a:
+    elif a != b != c != a:
         return ("разносторонний")
     else:
         return ("разносторонний")
+
+
+def division(a, b):
+    c = a / b
+    return c
+
+
+#print(division(30, 6))
+def sort(lst):
+    if sorted(lst)==lst:
+        print("Список отсортирован")
+    else:
+        print("Список не отсортирован")
+

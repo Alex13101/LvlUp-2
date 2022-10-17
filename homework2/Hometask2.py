e5012b57649fca4cdfb9bfbbf3ae2bbd9e73d432
Alex13101 <podiyachiy-a@mail.ru>
2022-10-17 21:14:12 +0300

    Calculate

--- homework2/Hometask2.py
+++ homework2/Hometask2.py
@@ -9,3 +9,25 @@
 5 + 6 = 0
 10 / 3 = 3.33333 # Можно округлять как удобно
 6 / 0 - на ноль делить нельзя """
+
+def read_input ():
+    a=int(input())
+    b=int(input())
+    c=input()
+    return a,b,c
+
+def calculate (a,b,c):
+
+    if c=="+":
+        print(a+b)
+    elif c=="-":
+        print(a-b)
+    elif c=="*":
+        print(a*b)
+    elif c=="/":
+        if b==0:
+            print("На ноль делить нельзя")
+        else:
+            print(a / b)
+a,b,c=read_input()
+calculate(a,b,c)

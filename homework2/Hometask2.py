e1f9700db3828f82ac6df858609bc74272748fba
Alex13101 <podiyachiy-a@mail.ru>
2022-10-18 16:37:28 +0300

    Calculate (Только целые числа)

--- homework2/Hometask2.py
+++ homework2/Hometask2.py
@@ -10,24 +10,28 @@
 10 / 3 = 3.33333 # Можно округлять как удобно
 6 / 0 - на ноль делить нельзя """
 
-def read_input ():
-    a=int(input())
-    b=int(input())
-    c=input()
-    return a,b,c
 
-def calculate (a,b,c):
+def read_input():
+    a, c, b = input("Введите пример в формате х+у: ")
+    a = int(a)
+    b = int(b)
+    return a, c, b
 
-    if c=="+":
-        print(a+b)
-    elif c=="-":
-        print(a-b)
-    elif c=="*":
-        print(a*b)
-    elif c=="/":
-        if b==0:
+
+def calculate(a, c, b):
+    if c == "+":
+        print(f" {a} + {b} = ",  round((a+b),2))
+    elif c == "-":
+        print(f"{a} - {b} = ", a-b)
+    elif c == "*":
+        print(f"{a} * {b} = ", a*b)
+    elif c == "/":
+        if b == 0:
             print("На ноль делить нельзя")
         else:
-            print(a / b)
-a,b,c=read_input()
-calculate(a,b,c)
+            print(f"{a} / {b} = ", round((a/b),2))
+
+
+a, c, b = read_input()
+#print(read_input())
+calculate(a, c, b)

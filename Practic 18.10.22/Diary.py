b4f1bd4b2b844a080f93e77a74ed5aa7c171ba1a
Alex13101 <podiyachiy-a@mail.ru>
2022-10-20 23:46:15 +0300

    Notebook

--- Practic 18.10.22/Diary.py
+++ Practic 18.10.22/Diary.py
@@ -3,7 +3,11 @@ from typing import Dict, Any
 contacts = {"Вася": 123, "Петя": 223}
 
 while True:
-    command = input()
+
+
+    print("exit", "get_contact \n",
+        "add_contact","print", "delete" )
+    command = input("Введите команду: ")
     if command == "exit":
         break
     elif command == "get_contact":
@@ -12,14 +16,24 @@ while True:
             print(contacts[name])
 
         else:
-            print("Такого контакта не существует")
+            print('Такого контакта не существует')
     elif command == "add_contact":
 
         name = input("Ведите имя: ")
         phone = int(input(" Ведите телефон: "))
         contacts[name] = phone
 
-        print(name)
+    elif command == "print":
+
+        print(contacts)
+    elif command == "delete":
+        name = input("Введите имя абонента: ")
+        if name in contacts:
+            print("Удалено!")
+            del contacts[name]
+            print(contacts)
+        else:
+            print('Такой записи нет')
         # Здесь нужно реализовать добавление контакта в список контактов
         ...
     else:

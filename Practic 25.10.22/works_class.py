6db77f6638fc9189360343120aaa8b0affc32ee6
Alex13101 <podiyachiy-a@mail.ru>
2022-10-25 22:21:35 +0300

    My_car

--- Practic 25.10.22/works_class.py
+++ Practic 25.10.22/works_class.py
@@ -2,14 +2,21 @@ class my_car:
     def __init__(self, color, name, mileage):
         self.color = color
         self.name = name
-        self.mileage = mileage
+        self.mileage_1 = mileage
 
     def mileage(self):
-        self.mileage += input("Сколько проехали?:  ")
-        print("Машина прошла ", self.mileage())
+        n = int(input("Сколько проехали?:"))
+        self.mileage_1 += n
+        print(f"Машина прошла {self.name} цвета {self.color} проехала {self.mileage_1}")
 
 
-my_car1 = my_car("red", "toyota", 0)
-my_car2 = my_car("blue", "Lada", 0)
+my_car1 = my_car("red", "toyota", 1000)
+my_car2 = my_car("blue", "Lada", 1600)
+my_car3 = my_car("green", "iveco", 500)
 
-my_car1.mileage(my_car2)
+
+my_car1.mileage()
+my_car2.mileage()
+my_car3.mileage()
+
+

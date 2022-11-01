0565a775a9cff1667e624f05ec26465166d5ff70
Alex13101 <podiyachiy-a@mail.ru>
2022-11-01 17:47:20 +0300

    utils for registrations Запичь и чтение из файла не работает

--- Practic 28.10.22/utils.py
+++ Practic 28.10.22/utils.py
@@ -18,3 +18,20 @@ def is_login_correct(login, password, users):
                 and user.password == password:
             return True
     return False
+
+
+def save_users (login, password):
+    with open("data.txt", "a") as f:
+        if not is_login_correct(login, password):
+            f.write(login, password)
+
+
+def get_users():
+    with open("data.txt", "r") as f:
+        lines = f.readlines()
+        # login password mode
+        for line in lines:
+            splited_line = line.split() #["login", "password", "mode"]
+            login = splited_line[0]
+            User(login, password, mode)
+

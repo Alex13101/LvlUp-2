d4a0413138025fb21478f377cbd964f8f4c1e2e1
Alex13101 <podiyachiy-a@mail.ru>
2022-11-05 18:04:37 +0300

    Project

---
+++ Project_!!!!/news_bot.py
@@ -0,0 +1,13 @@
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5766012834:AAHgsslpBBbumdkKCZ_b5X0HsJwc5FBsrH4') #Токен от bot_father
+
+
+
+
+
+
+
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
